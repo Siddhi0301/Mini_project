c2a7359c912a3bfe47a51f6b526c9b0288614e01
Snehal Ajit Mahasagar <snehalmahasagar@gmail.com>
2024-04-09 23:12:52 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,51 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import { logo } from "../assets";
 
 const Hero = () => {
+  const { loginWithRedirect, isAuthenticated,logout ,user} = useAuth0();
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
-
-        <button
+          
+            {isAuthenticated && (
+            <li>
+            <p>
+              {user.name}
+              </p>
+              </li>
+              )}
+          
+        {
+          isAuthenticated ? (
+            <li>
+              <button 
+        type='button' 
+        onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
+        className='black_btn' 
+        >
+             Log Out
+          </button>
+            </li>
+          ):
+          (
+            <li>
+              <button
           type='button'
-          onClick={() =>
-            window.open("https://github.com/Snehalmahasagar", "_blank")
+          onClick={() =>loginWithRedirect()
           }
           className='black_btn'
         >
-          GitHub
+          Log In
         </button>
-      </nav>
+            </li>
+          )
+        }
+
+        
+            </nav>
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,21 @@ import { Provider } from "react-redux";
 
 import App from "./App";
 import { store } from "./services/store";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Auth0Provider
+  domain="dev-kw8ijqw5rmvrptuj.us.auth0.com"
+  clientId="2KXZSrV3ZqauqpdJk7OxRm9T5qhPr5NF"
+  authorizationParams={{
+    redirect_uri: window.location.origin}}>
+      <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+
+ 
+</Auth0Provider>
+    
 );
+
+  
