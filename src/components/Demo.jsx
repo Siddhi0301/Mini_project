26b522de26d2ddc287e44254c76d8241630e682f
Snehal Ajit Mahasagar <116059585+Snehalmahasagar@users.noreply.github.com>
2024-04-15 08:26:37 +0530

    Update Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import microphone from "../assets/microphone.svg";
 import stopicon from "../assets/stopicon.svg";
-import shareIcon from "../assets/share.svg";
+import share from "../assets/share.svg";
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
