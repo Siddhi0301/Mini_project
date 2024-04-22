10f56c1ce799d1fc6a8c99fe4a0fe327d0b17e5b
Snehal Ajit Mahasagar <snehalmahasagar@gmail.com>
2024-04-23 03:33:41 +0530

    first commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,11 +3,7 @@ import loader from './loader.svg'
 import copy from './copy.svg'
 import logo from './logo.svg'
 import tick from './tick.svg'
-import eyeicon from './eyeicon.svg'
-import stopicon from './stopicon.svg'
-import microphone from './microphone.svg'
-import googleLogo from './googleLogo.png'
-import share from './share.svg'
+
 
 
 export {
@@ -15,11 +11,7 @@ export {
     loader,
     copy,
     logo,
-    eyeicon,
-    stopicon ,
-    microphone,
-    googleLogo,
-    share,
+   
 
     
     tick
